d27b75550c07b6a960c31952eff7e74ebfc8880e
fo0s <fo0sies@gmail.com>
2019-07-04 12:11:24 +0100

    Add: App can take an input

--- Ruby/lib/sudoku.rb
+++ Ruby/lib/sudoku.rb
@@ -1,9 +1,13 @@
 # Main sudoku solver class
 
 class Sudoku
-  attr_reader :output
+  attr_reader :output, :initial_input
 
   def initialize
     @output = []
+  end
+
+  def to_solve(input)
+    @initial_input = input
   end
 end
--- Ruby/spec/sudoku_spec.rb
+++ Ruby/spec/sudoku_spec.rb
@@ -2,10 +2,15 @@ require 'sudoku'
 
 # Main spec file
 describe 'Sudoku solver' do
+  before(:each) { @game = Sudoku.new }
   context '#Start--' do
     it 'can start' do
-      @game = Sudoku.new
       expect(@game.output).to eq []
+    end
+
+    it 'can take an input' do
+      @game.to_solve([1])
+      expect(@game.initial_input).to eq [1]
     end
   end
 end
